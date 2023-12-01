ca6676bc137ebfe6133a6ebad922b4af12c57a8f
Emiliano Navarro <emilianonavarro1993@gmail.com>
2023-12-01 03:53:24 -0300

    funcional

--- index.js
+++ index.js
@@ -2,9 +2,6 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config();
-}
 
 app.use(express.static("dist"));
 
